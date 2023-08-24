b7ae67cc7ac6c97a61dc34cf64c1d3727528f7bd
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-08-24 15:38:41 +0900

    1.1のレシピをコミット

--- chap01/1.1/server.js
+++ chap01/1.1/server.js
@@ -1,8 +1,38 @@
 var http = require("http");
+var path = require("path");
+
+var pages = [
+  { route: "", output: "Woohoo!" },
+  { route: "about", output: "シンプルなサンプルコードです。" },
+  {
+    route: "another page",
+    output: function () {
+      return "これが" + this.route;
+    },
+  },
+];
 
 http
   .createServer(function (request, response) {
-    response.writeHead(200, { "Content-type": "text/html" });
-    response.end("Woohoo!");
+    var lookup = path.basename(decodeURI(request.url));
+    pages.forEach(function (page) {
+      if (page.route == lookup) {
+        response.writeHead(200, {
+          "Content-type": "text/html;charset=utf-8",
+        });
+        response.end(
+          typeof page.output === "function" ? page.output() : page.output
+        );
+      }
+    });
+    // response.finishedは非推奨のためwritableEndedを使用する
+    // https://nodejs.org/docs/latest-v20.x/api/http.html#class-httpserverresponse
+    // if (!response.finished) {
+    if (!response.writableEnded) {
+      response.writeHead(404, {
+        "Content-Type": "text/html; charset=utf-8",
+      });
+      response.end("ページが見つかりません！");
+    }
   })
   .listen(8080);
